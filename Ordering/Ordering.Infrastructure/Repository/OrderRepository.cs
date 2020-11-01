2ec3d44caf991f97fb6e8485c90bd2fc84501bc1
Ibrahim <ebuo12@gmail.com>
2020-11-01 21:04:16 +0100

    cqrs is done

---
+++ Ordering/Ordering.Infrastructure/Repository/OrderRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Enitites;
+using Ordering.Core.Repository;
+using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Repository.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repository
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext dbcontext) : base(dbcontext)
+        {
+
+        }
+
+        public async Task<IEnumerable<Order>> GetOderByUserName(string userName)
+        {
+            var orderList = await _dbContext.Orders
+                                        .Where(o => o.UserName == userName)
+                                        .ToListAsync();
+            return orderList;
+        }
+    }
+}
