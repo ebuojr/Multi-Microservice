8139be4a704b7518997b4ffefaa69d18670539a5
Ibrahim <ebuo12@gmail.com>
2020-11-01 19:50:06 +0100

    OrderAPi + Sql

---
+++ Ordering/Ordering.Core/Enitites/Base/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Core.Enitites.Base
+{
+    public abstract class Entity : EntityBase<int>
+    {
+
+    }
+}
---
+++ Ordering/Ordering.Core/Repository/IOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using Ordering.Core.Enitites;
+using Ordering.Core.Repository.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Core.Repository
+{
+    public interface IOrderRepository : IRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOderByUserName(string userName);
+        
+    }
+}
